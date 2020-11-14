c2d44189931db5a481945f4eda7c54b5caccdc0f
WangZiHong <1215232494@qq.com>
2020-11-14 14:48:20 +0800

    新增button控件

--- Homeworks_5/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ Homeworks_5/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -1,11 +1,34 @@
 package com.example.uiwidgettest;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+
+                break;
+            default:
+                break;
+        }
+    }
 
 }
