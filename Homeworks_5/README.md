2d3c6a77cfd2e72eaa11e1c11d1b6e89af6791a4
WangZiHong <1215232494@qq.com>
2020-11-14 14:46:10 +0800

    新建作业5，新增TextView

---
+++ Homeworks_5/README.md
@@ -0,0 +1,7 @@
+# 基本UI控件和布局，以及他们的事件回调函数应用。
+
+
+
+实现界面如图
+
+从上到下分别是TextView、Button、EditText、ImageView、
---
+++ Homeworks_5/UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.uiwidgettest;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+}
