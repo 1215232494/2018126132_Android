4be3d108bd2d42bcb749e51d3a99d006e031bf0e
WangZiHong <1215232494@qq.com>
2020-11-22 23:30:31 +0800

    实现进度显示

--- Homeworks_8/AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
+++ Homeworks_8/AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.java
@@ -2,24 +2,137 @@ package com.example.asynctask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
+
+    // 线程变量
+    MyTask mTask;
+
+    // 主布局中的UI组件
+    Button button,cancel; // 加载、取消按钮
+    TextView text; // 更新的UI组件
+    ProgressBar progressBar; // 进度条
+
+    private class MyTask extends AsyncTask<String, Integer, Boolean> {
+
+        // 方法1：onPreExecute（）
+        // 作用：执行 线程任务前的操作
+        @Override
+        protected void onPreExecute() {
+            progressDialog.show();
+            text.setText("下载中");
+            // 执行前显示提示
+        }
+
+
+        // 方法2：doInBackground（）
+        // 作用：接收输入参数、执行任务中的耗时操作、返回 线程任务执行的结果
+        // 此处通过计算从而模拟“加载进度”的情况
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            try {
+                int count = 0;
+                int length = 1;
+                while (count<99) {
+
+                    count += length;
+                    // 可调用publishProgress（）显示进度, 之后将执行onProgressUpdate（）
+                    publishProgress(count);
+                    // 模拟耗时任务
+                    Thread.sleep(50);
+                }
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+            return true;
+        }
+
+        // 方法3：onProgressUpdate（）
+        // 作用：在主线程 显示线程任务执行的进度
+        @Override
+        protected void onProgressUpdate(Integer... progresses) {
+            progressDialog.setMessage("Downloaded" + progresses[0] + "%");
+
+            progressBar.setProgress(progresses[0]);
+            text.setText("loading..." + progresses[0] + "%");
+
+        }
+
+        // 方法4：onPostExecute（）
+        // 作用：接收线程任务执行结果、将执行结果显示到UI组件
+        @Override
+        protected void onPostExecute(Boolean result) {
+            progressDialog.dismiss();
+            if (result){ // 下载结果
+                Toast.makeText(getApplicationContext(), "下载成功！",Toast.LENGTH_SHORT).show();
+                text.setText("下载成功");
+            }else {
+                Toast.makeText(getApplicationContext(), "下载失败!",Toast.LENGTH_SHORT).show();
+                text.setText("下载失败");
+            }
+
+        }
+
+        // 方法5：onCancelled()
+        // 作用：将异步任务设置为：取消状态
+        @Override
+        protected void onCancelled() {
+
+            text.setText("已取消");
+            Toast.makeText(getApplicationContext(), "已取消!",Toast.LENGTH_SHORT).show();
+            progressBar.setProgress(0);
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+         progressDialog = new ProgressDialog(this);
+
+        // 绑定UI组件
         setContentView(R.layout.activity_main);
-        TextView text = (TextView) findViewById(R.id.text);
-        Button download = (Button) findViewById(R.id.download);
-        download.setOnClickListener(this);
+
+        button = (Button) findViewById(R.id.button);
+        cancel = (Button) findViewById(R.id.cancel);
+        text = (TextView) findViewById(R.id.text);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+        mTask = new MyTask();
+
+        // 加载按钮按按下时，则启动AsyncTask
+        // 任务完成后更新TextView的文本
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTask.execute();
+            }
+        });
+
+        cancel = (Button) findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 取消一个正在执行的任务,onCancelled方法将会被调用
+                mTask.cancel(true);
+            }
+        });
+
     }
 
-    @Override
-    public void onClick(View v) {
-        new DownloadTask().execute();
-    }
 }
