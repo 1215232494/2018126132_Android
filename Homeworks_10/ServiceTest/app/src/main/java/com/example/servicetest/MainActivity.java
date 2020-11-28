31362e5abb1a7576cc2c87bef5a077c15bfcd25e
WangZiHong <1215232494@qq.com>
2020-11-28 23:49:41 +0800

    添加活动与服务的绑定/解绑按钮

--- Homeworks_10/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ Homeworks_10/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.servicetest;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 
 
@@ -12,8 +14,15 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bindService = (Button) findViewById(R.id.bind_service);
+        Button unbindService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unbindService.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        
     }
 
 }
