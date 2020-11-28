b7eb787f6bf61403fc6196983f13d17dcfa9313a
WangZiHong <1215232494@qq.com>
2020-11-28 23:46:05 +0800

    创建服务

---
+++ Homeworks_10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -0,0 +1,35 @@
+package com.example.servicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class MyService extends Service {
+
+    public MyService() {
+    }
+
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService", "onDestroy executed");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
