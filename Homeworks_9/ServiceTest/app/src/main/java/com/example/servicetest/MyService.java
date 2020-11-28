97f4ece3fb75f02b26413d238aa3775acb3f74bf
WangZiHong <1215232494@qq.com>
2020-11-28 22:55:10 +0800

    实现按钮开关服务功能

--- Homeworks_9/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ Homeworks_9/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -3,6 +3,7 @@ package com.example.servicetest;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -10,6 +11,27 @@ import androidx.annotation.Nullable;
 public class MyService extends Service {
 
     public MyService() {
+    }
+
+
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("MyService", "onCreate executed");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand executed");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyService", "onDestroy executed");
     }
 
     @Nullable
