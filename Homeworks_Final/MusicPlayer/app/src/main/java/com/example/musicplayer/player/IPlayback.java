2f9ce2bb13cbc57b4c0482d05caf113115f3ba06
WangZiHong <1215232494@qq.com>
2020-12-14 13:04:32 +0800

    播放音乐时更新进度条

---
+++ Homeworks_Final/MusicPlayer/app/src/main/java/com/example/musicplayer/player/IPlayback.java
@@ -0,0 +1,55 @@
+package com.example.musicplayer.player;
+
+
+import androidx.annotation.Nullable;
+
+import com.example.musicplayer.data.model.PlayList;
+import com.example.musicplayer.data.model.Song;
+
+public interface IPlayback {
+
+    void setPlayList(PlayList list);
+
+    boolean play();
+
+    boolean play(PlayList list);
+
+    boolean play(PlayList list, int startIndex);
+
+    boolean play(Song song);
+
+    boolean playLast();
+
+    boolean playNext();
+
+    boolean pause();
+
+    boolean isPlaying();
+
+    int getProgress();
+
+    Song getPlayingSong();
+
+    boolean seekTo(int progress);
+
+    void setPlayMode(PlayMode playMode);
+
+    void registerCallback(Callback callback);
+
+    void unregisterCallback(Callback callback);
+
+    void removeCallbacks();
+
+    void releasePlayer();
+
+    interface Callback {
+
+        void onSwitchLast(@Nullable Song last);
+
+        void onSwitchNext(@Nullable Song next);
+
+        void onComplete(@Nullable Song next);
+
+        void onPlayStatusChanged(boolean isPlaying);
+    }
+}
